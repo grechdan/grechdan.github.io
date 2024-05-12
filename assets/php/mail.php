34122b945383d0e61e83bd9d60a4c2a6ea024924
grechdan <daniil.grechanyy@gmail.com>
2024-05-12 23:09:08 +0200

    new page per.html

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,13 +1,14 @@
 <?php
 
-$to 		= 'tamjid.murtoza@gmail.com';
+$to 		= 'daniil.grechanyy@gmail.com';
 $headers	= 'FROM: "'.$email.'"';
 
 //All form values
 $name 		= $_POST['name'];
+$name 		= $_POST['phone'];
 $email 		= $_POST['email'];
 $subject 	= $_POST['subject'];
 $msg 		= $_POST['msg'];
-$output 	= "Name: ".$name."\nEmail: ".$email."\nSubject: ".$subject."\n\nMessage: ".$msg;
+$output 	= "Name: ".$name."\nPhone: ".$name."\nEmail: ".$email."\nSubject: ".$subject."\n\nMessage: ".$msg;
 
 $send		= mail($to, $name, $output, $headers);
